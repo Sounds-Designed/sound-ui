7ccde8ec573051f1f7d7076f21f494eb0a591b62
Alex Scott <codetheorist@outlook.com>
2026-01-16 14:19:37 +0000

    feat: WIP improve CLI tool

--- cli/commands/make/index.mjs
+++ cli/commands/make/index.mjs
@@ -1,4 +1,5 @@
 import { defineCommand } from 'citty'
+import component from './component.mjs'
 import locale from './locale.mjs'
 
 export default defineCommand({
@@ -7,6 +8,7 @@ export default defineCommand({
     description: 'Commands to create new Nuxt UI entities.'
   },
   subCommands: {
+    component,
     locale
   }
 })
